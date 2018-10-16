afcd20e67437d4cdebd70062e5945e1a184f5993
Francisco Sanchez <fsancheztemprano@danielcastelao.org>
2018-10-16 08:57:38 +0200

    commit inicial

---
+++ cod_1/COD_1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cod_1;
+
+/**
+ * @author fsancheztemprano
+ */
+public class COD_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Comentario 1 fsancheztemprano
+    }
+    
+}
